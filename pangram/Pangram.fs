1abc900cb732dcc34c7cfabdd2a698ea2384e7b8
João Vitor Retamero <joaovretamero@gmail.com>
2018-10-11 23:57:14 -0300

    Pangram

---
+++ pangram/Pangram.fs
@@ -0,0 +1,6 @@
+﻿module Pangram
+
+let isPangram (input: string): bool =
+    let alphabet = ['a'..'z']
+    let lowerInput = input.ToLower()
+    Seq.forall (fun letter -> lowerInput.IndexOfAny [|letter|] >= 0) alphabet
