795ab872075f81e42a48687c898c7a2fe08e1190
João Vitor Retamero <joaovretamero@gmail.com>
2018-10-11 23:29:45 -0300

    Hello World and Hamming exercises

---
+++ hamming/Hamming.fs
@@ -0,0 +1,17 @@
+﻿module Hamming
+
+let pairs firstStrand secondStrand =
+    List.zip (Seq.toList firstStrand) (Seq.toList secondStrand)
+
+let hamming pairs =
+    List.fold
+        (fun acc (leftItem, rightItem) ->
+            acc + if leftItem.Equals rightItem then 0 else 1) 0 pairs
+
+let distance (strand1: string) (strand2: string): int option =
+    if strand1.Length = 0 || strand1.Length = 0 then
+        Some 0
+    else if strand1.Length = strand2.Length then
+        Some (hamming (pairs strand1 strand2))
+    else
+        None
