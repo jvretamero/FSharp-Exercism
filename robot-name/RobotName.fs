1237c2fa4987ba267f59a637c171b85185d5dc4e
João Vitor Retamero <joaovretamero@gmail.com>
2018-10-12 15:29:52 -0300

    RobotName

---
+++ robot-name/RobotName.fs
@@ -0,0 +1,21 @@
+﻿module RobotName
+
+let letters = [|'A'..'Z'|]
+let numbers = [|'0'..'9'|]
+
+let randomItems (items: 'a array) count =
+    let random = System.Random()
+    let nextIndex() = random.Next(items.Length)
+    List.init count (fun i -> items.[nextIndex()])
+
+let toString (items: char list) = items |> List.toArray |> System.String
+
+let randomString items count = randomItems items count |> toString
+
+type Robot = {Name: string}
+
+let mkRobot() = { Robot.Name = (randomString letters 3) + (randomString numbers 4) }
+
+let name robot = robot.Name
+
+let reset robot = mkRobot()
