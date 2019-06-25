6260b88df955f3635fdd18ba21b0d6c9e07b8929
João Vitor Retamero <joaovretamero@gmail.com>
2019-06-25 20:54:32 -0300

    Largest Series Product problem & solution :grin:

---
+++ largest-series-product/LargestSeriesProduct.fs
@@ -0,0 +1,20 @@
+﻿module LargestSeriesProduct
+
+open System
+
+let largestProduct (input: string) seriesLength : int option =
+    if Seq.exists (Char.IsDigit >> not) input then
+        None
+    else
+        match input.Length with
+        | len when len < seriesLength -> None
+        | 0 when seriesLength = 0 -> Some 1
+        | _ when seriesLength = 0 -> Some 1
+        | _ when seriesLength < 0 -> None
+        | _ ->
+                input
+                |> Seq.map (string >> int)
+                |> Seq.windowed seriesLength
+                |> Seq.map (Seq.reduce ( * ))
+                |> Seq.max
+                |> Some
