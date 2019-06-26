5b5f6cf487c8c9faccaba29213ca6535bcda0d60
João Vitor Retamero <joaovretamero@gmail.com>
2019-06-25 22:11:41 -0300

    Nth Prime solution

--- nth-prime/NthPrime.fs
+++ nth-prime/NthPrime.fs
@@ -1,3 +1,21 @@
 ﻿module NthPrime
 
-let prime nth : int option = failwith "You need to implement this function."
+let isPrime x =
+    let max = (float >> sqrt >> int) x
+    [ 2 .. max ]
+    |> List.forall (fun y -> x % y <> 0) 
+
+let allPrimes =
+    let rec recPrimes n =
+        seq {
+            if isPrime n then
+                yield n
+            
+            yield! recPrimes (n + 1)
+        }
+    recPrimes 2
+
+let prime nth : int option =
+    match nth with
+    | 0 -> None
+    | n -> Some (allPrimes |> Seq.item (n - 1))
